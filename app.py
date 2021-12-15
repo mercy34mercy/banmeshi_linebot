4f6c0e07f0bf009d9054c5e559816cb030c055d3
mercy34mercy <masakun@DESKTOP-PLC0L1Q.localdomain>
2021-12-15 18:53:18 +0900

    Ike

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def handle_message(event):
     recipe = get_recipe(event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(event.message.text))
+        TextSendMessage(text = recipe))
 
 if __name__ == "__main__":
     app.run()
