4e2987579fda68fca11bc760697260f43171738a
mercy34mercy <masakun@DESKTOP-PLC0L1Q.localdomain>
2021-12-15 15:21:32 +0900

    ike

--- request.py
+++ request.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def get_recipe(postdata):
     url = "https://banmeshii.herokuapp.com/get_db_recipe_one"
@@ -14,7 +15,7 @@ def get_recipe(postdata):
     print(datas)
 
 
-    data = requests.post(url,datas)
+    data = requests.post(url,data = json.dumps(datas))
 
     print(data)
 
