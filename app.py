7462fa6eea82c6c7dd86e035be93a9fee9ff998b
mercy34mercy <masakun@DESKTOP-PLC0L1Q.localdomain>
2021-12-15 19:05:07 +0900

    終了

--- app.py
+++ app.py
@@ -41,11 +41,12 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     #関数呼び出し
     recipe = get_recipe(event.message.text)
-    
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text = recipe))
