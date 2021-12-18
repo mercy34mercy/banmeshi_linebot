8ac361faa13a80d6534650318b7da6b111177b08
unknown <syunyasisotto@gmail.com>
2021-12-18 21:40:24 +0900

    どこがどうなってるのか解読してた

--- request.py
+++ request.py
@@ -3,26 +3,27 @@ import json
 
 def get_recipe(postdata):
 
-    for i in range(1,3):
-        #banmeshiAPIのURL
-        url = "https://banmeshii.herokuapp.com/get_db_recipe_one"
 
-        #jsonの型宣言
-        datas = ({
-            "data":[]
-        })
+    #banmeshiAPIのURL
+    url = "https://banmeshii.herokuapp.com/get_db_recipe_one"
 
-        #Lineから貰ってきたデータを配列に入れる
-        datas["data"].append(str(postdata))
+    #jsonの型宣言
+    datas = ({
+        "data":[]
+    })
 
-        #BanmeshiAPIにリクエスト
-        data = requests.post(url,json = datas).text
+    #Lineから貰ってきたデータを配列に入れる
+    datas["data"].append(str(postdata))
 
-    #json文字列を辞書に変換
-        data_ = json.loads(data)
+    #BanmeshiAPIにリクエスト
+    data = requests.post(url,json = datas).text
 
-        returndata = data_["data"][i]["foodImageUrl"]
+#json文字列を辞書に変換
+    data_ = json.loads(data)
+    
+    returndata = data_["data"][0]["foodImageUrl"]["foodImage"]
+    
 
-        print(returndata)
+    print(returndata)
 
-        return returndata
+    return returndata
