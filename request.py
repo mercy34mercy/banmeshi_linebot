a8ea79d93f329543883d3b30f8883ce65b633c77
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2022-06-15 22:46:42 +0900

    レシピがない時の処理追加

--- request.py
+++ request.py
@@ -21,12 +21,13 @@ def get_recipe(postdata):
 
     #BanmeshiAPIにリクエスト
     data = requests.post(url,json = datas).text
-
     
 
 #json文字列を辞書に変換
     data_ = json.loads(data)
-    print(data_)
+    if(len(data_["data"]) == 0):
+        return("レシピが存在しません")
+    
     # for i in range(0,2):
     #     returndata =[ data_["data"][0]["foodImageUrl"],data_["data"][1]["recipeUrl"]]
         
@@ -37,10 +38,7 @@ def get_recipe(postdata):
             "url":data_["data"][0]["recipeUrl"]
     }
     returnlist.append(result_dict)
-        # print(returndata)
-        # print(returndata2)
-        # print(data)
-        # print(returndata[i])
-    # print(data[:100])
     print(returnlist)
+
     return str(data_["data"][0]["recipeUrl"])
+    
