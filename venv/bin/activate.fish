e85f948381b7d583d855fd52e12da9688808f734
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2022-06-15 21:37:48 +0900

    仮想環境作り直し

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/masakun/py_pro/linebot2/venv"
+set -gx VIRTUAL_ENV "/home/makun/programing/python/banmeshi_linebot/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
